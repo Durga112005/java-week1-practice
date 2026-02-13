ba8df660c9b730b1752efa160b15ab10e1b30a7f
Durgadevi S <durgadevi112005@gmail.com>
2026-02-13 21:27:51 +0530

    Add Largest of 3 Numbers

---
+++ LargestofThree.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class LargestofThree {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int largest  = a;
+
+        if(b>largest){
+            largest = b;
+        }
+        if(c>largest){
+            largest = c;
+        }
+        System.out.println(largest + " " + "is Largest");
+
+        sc.close();
+    }
+    
+}
